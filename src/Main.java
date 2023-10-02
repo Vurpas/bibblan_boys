7376e2cc9dedb62ee1ed47e6a24b2ace40fea683
Oskar Gester <gester.oskar@gmail.com>
2023-10-02 14:58:16 +0200

    uppdaterat describe book, ny uppdaterad  arraylist, fungerande foreach loop som visar hela boklistan, samt borrow och return book metoder

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -6,26 +7,33 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Välkommen till Bibblan");
 
-        ArrayList<books> bookList = new ArrayList<>();
+        List<books> bookList = new ArrayList<>();
 
-        books book1 = new books("Lord of the rings", "J.R.R. Tolkien - ", "A magical ring needs to be destroyed to save the world");
-        books book2 = new books("Wuthering Heights", "Emily Bronte - ", "A love story about two young british people");
-        books book3 = new books("The Process", "Franz Kafka - ", "A man gets framed for a crime he did not commit");
-        books book4 = new books("Harry Potter", "J.K. Rowling - ", "A magical boy with glasses is trying to save the world");
-        books book5 = new books("Lord of the flies", "William Golding", "A group of boys gets stranded on a desert island");
-        books book6 = new books("The jungle book", "Rudyard Kipling", "A boy grows up in the jungle and is raised by wild animals");
-        books book7 = new books("Little women", "Louisa May Alcott", "A group of women live their lives during the American civil war");
-        books book8 = new books("Frankenstein", "Mary Shelley", "A young scientist ventures to far in his quest to create life");
-        books book9 = new books("The three musketeers", "Alexandre Dumas", "Three french guys walks around cutting people with swords");
-        books book10 = new books("The great Gatsby", "F. Scott Fitzgerald", "Party like theres no tomorrow with mr. Gatsby in the 1920s");
+        bookList.add(new books("Lord of the rings", "J.R.R. Tolkien - ", "A magical ring needs to be destroyed to save the world",true));
+        bookList.add(new books("Wuthering Heights", "Emily Bronte - ", "A love story about two young british people",true));
+        bookList.add(new books("The Process", "Franz Kafka - ", "A man gets framed for a crime he did not commit",true));
+        bookList.add(new books("Harry Potter", "J.K. Rowling - ", "A magical boy with glasses is trying to save the world",true));
+        bookList.add(new books("Lord of the flies", "William Golding", "A group of boys gets stranded on a desert island",true));
+        bookList.add(new books("The jungle book", "Rudyard Kipling", "A boy grows up in the jungle and is raised by wild animals",true));
+        bookList.add(new books("Little women", "Louisa May Alcott", "A group of women live their lives during the American civil war",true));
+        bookList.add(new books("Frankenstein", "Mary Shelley", "A young scientist ventures to far in his quest to create life",true));
+        bookList.add(new books("The three musketeers", "Alexandre Dumas", "Three french guys walks around cutting people with swords",true));
+        bookList.add(new books("The great Gatsby", "F. Scott Fitzgerald", "Party like theres no tomorrow with mr. Gatsby in the 1920s",true));
 
 
-        bookList.add(book1); bookList.add(book2); bookList.add(book3); bookList.add(book4); bookList.add(book5);
-        bookList.add(book6); bookList.add(book7); bookList.add(book8); bookList.add(book9); bookList.add(book10);
-        book1.describeBook();
-        for (books i : bookList) {
-            System.out.println(i);
-        }
+        //lånar ut bok baserat på angiven index plats, måste lånas ut innan aktuell forEach för att listan skall vara uppdaterad.
+        bookList.get(2).borrowBook();
+
+        //for each som visar varje bok i vår lista samt om den är available
+        bookList.forEach(books -> {
+            System.out.println("\nTitle : " + books.getTitle() + ", Author : " + books.getAuthor() + ", Info : " + books.getInfo() + "\nAvailable : " + books.isAvailable());
+        });
+
+
+        //bookList.get(1).borrowBook();
+        //bookList.get(1).describeBook();
+
+
 
 
     }
